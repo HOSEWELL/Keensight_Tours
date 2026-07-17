602b9c748232324f25f7f4a31d7f86d3b083ada4
HOSEWELL <hosewellkaranja@gmail.com>
2026-07-18 01:00:07 +0300

    renaming

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Safaridesk - Book your tickets online",
-  description: "Safaridesk helps you book tickets faster and easier online",
+  title: "Keensight Tours - Book your tickets online",
+  description: "Keensight Tours helps you book tickets faster and easier online",
   icons: {
     icon: "/logo.jpg",
   },
